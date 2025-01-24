083a756469160c41dc58b3aa6e6c3339b97f2942
Jean-Didier Pailleux <jean-didier.pailleux@sipearl.com>
2025-01-24 15:28:18 +0100

    [flang][PRIF] Add Lowering for THIS_IMAGE without coarray argument.

--- flang/include/flang/Optimizer/Builder/IntrinsicCall.h
+++ flang/include/flang/Optimizer/Builder/IntrinsicCall.h
@@ -444,6 +444,8 @@ struct IntrinsicLibrary {
   void genSystemClock(llvm::ArrayRef<fir::ExtendedValue>);
   mlir::Value genTand(mlir::Type, llvm::ArrayRef<mlir::Value>);
   mlir::Value genTime(mlir::Type, llvm::ArrayRef<mlir::Value>);
+  fir::ExtendedValue genThisImage(mlir::Type,
+                                  llvm::ArrayRef<fir::ExtendedValue>);
   mlir::Value genTrailz(mlir::Type, llvm::ArrayRef<mlir::Value>);
   fir::ExtendedValue genTransfer(mlir::Type,
                                  llvm::ArrayRef<fir::ExtendedValue>);
